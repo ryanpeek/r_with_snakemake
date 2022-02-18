11a5e77e41e9750a80383c44d657c50dfa480abc
Ryan Peek <rapeek@ucdavis.edu>
2022-02-18 12:15:05 -0800

    update viz

--- src/smk4_visualize.R
+++ src/smk4_visualize.R
@@ -18,14 +18,15 @@ infile <- snakemake@input[[1]]
 #data <- read_csv(file = "data_clean/davis_clim_data.csv.gz")
 data <- read_csv(file = infile)
 
-# ddata <- read_csv(file = "data_raw/davis_daily_historical_1980_2021.csv", skip = 63) %>%
-#   clean_names() %>%
-#   mutate(date = ymd(date)) %>%
-#   rename(precip_mm=precip, air_max_c = air_max, air_min_c=min_7,
-#          soil_max_c = soil_max, soil_min_c=min_15,
-#          solar_wsqm = solar) %>%
-#   select(station, date, precip_mm, air_max_c:air_min_c, soil_max_c:soil_min_c, solar_wsqm)
-# ddata <- ddata %>% wateRshedTools::add_WYD("date")
+data_hist <- read_csv(file = "data_raw/davis_daily_historical_1980_2021.csv", skip = 63) %>%
+  clean_names() %>%
+  mutate(date = ymd(date)) %>%
+  rename(precip_mm=precip, air_max_c = air_max, air_min_c=min_7,
+         soil_max_c = soil_max, soil_min_c=min_15,
+         solar_wsqm = solar) %>%
+  select(station, date, precip_mm, air_max_c:air_min_c, soil_max_c:soil_min_c, solar_wsqm)
+data_hist <- data_hist %>% wateRshedTools::add_WYD("date")
+
 
 # Clean Data ----------------------------------------------------------------
 
@@ -53,30 +54,39 @@ ppt_daily <- data %>% filter(metric_id == "CT_Rain_Tot24") %>%
   # drop 2009 since it's incomplete
   filter(!WY==2009)
 
-# test
-# ppt_daily %>% ggplot(data=.) + geom_line(aes(date, tot_ppt_in), color="blue") 
+# bind with historical data
+ppt_daily_all <- full_join(data_hist, ppt_daily) %>% 
+  mutate(ppt_mm = coalesce(tot_ppt_mm, precip_mm),
+         station = coalesce(station, as.character(station_id))) %>% 
+  select(station, date, DOY, DOWY, WY, ppt_mm)
+
 
 # Make Monthly Precip -----------------------------------------------------
 
-ppt_mon <- ppt_daily %>%
+ppt_mon <- ppt_daily_all %>%
+  mutate(month = lubridate::month(date)) %>% 
   group_by(WY, month) %>%
-  summarize("tot_ppt_in"=sum(tot_ppt_in, na.rm = T))
+  summarize("tot_ppt_mm"=sum(ppt_mm, na.rm = T)) %>% 
+  mutate(mon_wy = factor(month, levels = c(10,11,12,1,2,3,4,5,6,7,8,9)))
 
-ppt_wk <- ppt_daily %>%
+ppt_wk <- ppt_daily_all %>%
+  mutate(week = week(date)) %>% 
   group_by(WY, week) %>%
-  summarize("tot_ppt_in"=sum(tot_ppt_in, na.rm = T))
+  summarize("tot_ppt_mm"=sum(ppt_mm, na.rm = T)) %>% 
+  mutate(wk_wy = factor(week, levels = c(40:53, 1:39)))
 
 # Plot Precip -------------------------------------------------------------
 
 # max ppt: barplot
 gg_ppt_mon <- ggplot() +
   geom_col(data=ppt_mon, 
-           aes(x=month, y=tot_ppt_in, group=month), 
+           aes(x=mon_wy, y=tot_ppt_mm, group=mon_wy), 
            fill="steelblue", alpha=0.8, color="skyblue") +
-  geom_col(data=ppt_mon %>% filter(WY==2022),
-           aes(x=month, y=tot_ppt_in, group=month), 
+  # current wy
+  geom_col(data=ppt_mon %>% filter(WY==as.integer(format(Sys.Date(), "%Y"))),
+           aes(x=mon_wy, y=tot_ppt_mm, group=mon_wy), 
            fill="coral", alpha=0.8, color="maroon") +
-  labs(x="Month", y="Total Precip (in)", subtitle="Precip by Water Year") + 
+  labs(x="Month", y="Total Precip (mm)", subtitle="Precip by Water Year") + 
   ggdark::dark_theme_bw() +
   theme(axis.text.x = element_text(angle=90, vjust = 0.5))+
   facet_wrap(.~WY)
@@ -84,10 +94,10 @@ gg_ppt_mon <- ggplot() +
 # weekly through time
 gg_ppt_wk <- ggplot() +
   geom_col(data=ppt_wk,
-           aes(x=week, y=tot_ppt_in, group=WY),
+           aes(x=wk_wy, y=tot_ppt_mm, group=WY),
            fill="steelblue", alpha=0.8, color="skyblue") +
-  geom_col(data=ppt_wk %>% filter(WY==2022),
-           aes(x=week, y=tot_ppt_in), 
+  geom_col(data=ppt_wk %>% filter(WY==as.integer(format(Sys.Date(), "%Y"))),
+           aes(x=wk_wy, y=tot_ppt_mm), 
            fill="coral", alpha=0.8, color="maroon") +
   labs(x="Weeks (by Water Year: Oct 1 - Sep 30)", y="Weekly Precip (in)", 
        title = "Davis CA: Precip by Week")+
@@ -96,13 +106,22 @@ gg_ppt_wk <- ggplot() +
   facet_wrap(.~WY)
 
 # Look at Fall only
-gg_ppt_daily <- ppt_daily %>% filter(month %in% c("10","11","12")) %>% ungroup() %>% 
+# gg_ppt_daily <- ppt_daily %>% filter(month %in% c("10","11","12")) %>% ungroup() %>% 
+#   ggplot() + geom_col(aes(x=as.factor(WY), y=tot_ppt_in, fill=month))+
+#   ggdark::dark_theme_classic() +
+#   ggthemes::scale_fill_few(palette = "Light", "Month") +
+#   labs(title = "Davis CA: Fall Precip", y="Precip (in)", x="Water Year",
+#        caption = glue("Data Source: <http://atm.ucdavis.edu/weather/> \nupdated: {Sys.Date()}"))
+
+# Winter 
+gg_ppt_daily <- ppt_daily %>% filter(month %in% c("10","11","12", "1", "2")) %>% ungroup() %>% 
   ggplot() + geom_col(aes(x=as.factor(WY), y=tot_ppt_in, fill=month))+
   ggdark::dark_theme_classic() +
   ggthemes::scale_fill_few(palette = "Light", "Month") +
-  labs(title = "Davis CA: Fall Precip", y="Precip (in)", x="Water Year",
+  labs(title = "Davis CA: Winter Precip", y="Precip (in)", x="Water Year",
        caption = glue("Data Source: <http://atm.ucdavis.edu/weather/> \nupdated: {Sys.Date()}"))
- 
+
+
 # combine
 plot_out <- gg_ppt_wk / gg_ppt_daily
 
